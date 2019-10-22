7d5a8a95598eb826dbfa3cfcb7f475f61b992f0b
Wyatt J Herkamp <wherkamp@kingtux.me>
2019-10-22 08:08:27 -0400

    Update and rename Diagonal_Diff.java to Solution.java

---
+++ src/main/java/my/random/Solution.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Scanner;
+class Solution {
+ public static void main(String args[]) {
+  int sum1 = 0;
+  int cop;
+  int sum = 0;
+  Scanner sc = new Scanner(System.in);
+  int n = sc.nextInt();
+  cop = n - 1;
+  int[][] arr = new int[n][n];
+  for (int i = 0; i < n; ++i) {
+   for (int j = 0; j < n; ++j) {
+    arr[i][j] = sc.nextInt();
+
+   }
+  }
+  for (int i = 0; i < n; ++i, --cop) {
+   sum += arr[i][i];
+   sum1 += arr[i][cop];
+  }
+  System.out.println(Math.abs(sum1 - sum));
+ }
+}
